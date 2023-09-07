f04e77ee653d27d75fb28519a08b1b04d3f50e5f
sggnology <sggnology@gmail.com>
2023-09-07 21:26:51 +0900

    [view] layout > main, nightSaver 경로 설정

--- src/page/layout/Main.js
+++ src/page/layout/Main.js
@@ -6,12 +6,12 @@ function Main(props) {
     <>
       <h3>안녕하세요. sggnology 장난감들 둘러보고 가셔유</h3>
       <ul>
-        <Link to="/product/1">
+        <Link to="/product/nightSaver">
           <div style={{
             border: '2px solid black',
             padding: 10
           }}>
-            <li>1번 night-saver</li>
+            <li>1번 night saver</li>
           </div>
         </Link>
         <Link to="/product/2">
