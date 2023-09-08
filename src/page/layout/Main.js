4ed11631fb3fa8db7c6fffeaec80502a3cb92cb8
sggnology <sggnology@gmail.com>
2023-09-08 22:02:23 +0900

    [service] main, 경로 수정

--- src/page/layout/Main.js
+++ src/page/layout/Main.js
@@ -1,21 +1,17 @@
 import {Link} from "react-router-dom";
-import "../../firebase-messaging-sw"
 
 function Main(props) {
   return (
     <>
       <h3>안녕하세요. sggnology 장난감들 둘러보고 가셔유</h3>
       <ul>
-        <Link to="/product/nightSaver">
+        <Link to="/nightSaver">
           <div style={{
             border: '2px solid black',
             padding: 10
           }}>
             <li>1번 night saver</li>
           </div>
-        </Link>
-        <Link to="/product/2">
-          <li>2번상품</li>
         </Link>
       </ul>
     </>
