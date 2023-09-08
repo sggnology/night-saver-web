da991b7fef9df4fe80762836024c3006a6856996
sggnology <sggnology@gmail.com>
2023-09-08 22:01:59 +0900

    [service] nightsaver, 라우팅 구성

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 // import './firebase-messaging-sw.js'
 import Header from "./page/layout/Header";
 import Main from "./page/layout/Main";
-import Product from "./page/layout/Product";
 import {Route, Routes} from "react-router-dom";
 import NotFound from "./page/exception/NotFound";
+import NightSaver from "./page/view/nightSaver/NightSaver";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Main/>}></Route>
-        <Route path="/product/:productName" element={<Product/>}></Route>
+        <Route path="/nightSaver" element={<NightSaver/>}></Route>
         {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
