75a9eac479bc50b42af542635f0a001ee59bc1d2
sggnology <sggnology@gmail.com>
2023-09-15 18:16:04 +0900

    [service] NightSaver, useState 는 UI 를 위해 사용하게끔 처리
    - 기존의 isGranted, token 등의 state 는 삭제
    - try/catch 로 알림에 관한 예외처리

--- src/page/view/nightSaver/NightSaver.js
+++ src/page/view/nightSaver/NightSaver.js
@@ -1,85 +1,110 @@
 import axios from "axios";
 import {useState} from "react";
-import {getMessaging, getToken, onMessage} from "firebase/messaging";
+import {getMessaging, getToken} from "firebase/messaging";
 import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHeIKYOoBwRWg8-TK-U7cK-zRX90DG05A",
-  authDomain: "night-saver.firebaseapp.com",
-  projectId: "night-saver",
-  storageBucket: "night-saver.appspot.com",
-  messagingSenderId: "1020784651851",
-  appId: "1:1020784651851:web:fef32def2547c69577a7ce",
-  measurementId: "G-ZFYRQ6J660"
+    apiKey: "AIzaSyCHeIKYOoBwRWg8-TK-U7cK-zRX90DG05A",
+    authDomain: "night-saver.firebaseapp.com",
+    projectId: "night-saver",
+    storageBucket: "night-saver.appspot.com",
+    messagingSenderId: "1020784651851",
+    appId: "1:1020784651851:web:fef32def2547c69577a7ce",
+    measurementId: "G-ZFYRQ6J660"
 }
 
 function NightSaver(props) {
 
-  const app = initializeApp(firebaseConfig);
-  const messaging = getMessaging(app);
+    const app = initializeApp(firebaseConfig);
+    const messaging = getMessaging(app);
 
-  const [isAlertPermissionGranted, setIsAlertPermissionGranted] = useState(false)
-  const [token, setToken] = useState(null)
+    const [tokenIssueLoading, setTokenIssueLoading] = useState(true);
 
-  const issueToken = async () => {
-    await requestAlertPermission()
-    await requestToken()
-    await saveToken()
-  }
+    const issueToken = async () => {
 
-  const requestAlertPermission = async () => {
-
-    const permission = await Notification.requestPermission()
-    const isPermissionGranted = permission === 'granted'
-    setIsAlertPermissionGranted(isPermissionGranted)
-
-    if (!isPermissionGranted) {
-      console.warn("알림을 허용하지 않았습니다.")
-      alert("알림을 허용하지 않아 정상적으로 동작하지 않습니다.")
+        try {
+            await requestAlertPermission();
+            const token = await requestToken();
+            await saveToken(token);
+            setTokenIssueLoading(false);
+        } catch (e) {
+            console.error("토큰 발행 과정에서 오류가 발생하였습니다.");
+            alert("토큰 발행을 실패하였습니다. 다시 시도하여주세요.");
+        }
     }
-  }
 
-  const requestToken = async () => {
-    if (isAlertPermissionGranted) {
-      const token = await getToken(messaging, {
-        vapidKey: process.env.REACT_APP_VAPID_KEY,
-      });
+    const requestAlertPermission = async () => {
 
-      console.log("토큰을 발행하였습니다. : ", token)
+        const permission = await Notification.requestPermission();
+        const isPermissionGranted = permission === 'granted';
 
-      setToken(token)
+        if (!isPermissionGranted) {
+            console.warn("알림을 허용하지 않았습니다.");
+            throw new Error("알림을 허용하지 않았습니다.");
+        }
     }
-  }
 
-  const saveToken = () => {
-    if(token !== null) {
-      const url = "http://localhost:8080/api/v1/token"
-      const body = {
-        token: token
-      }
+    const requestToken = async () => {
 
-      axios
-        .post(url, body)
-        .then((response) => {
-          console.log("토큰을 저장하였습니다.")
-        })
-        .catch((error) => {
-          console.error("토큰 저장에 실패하였습니다.")
-          alert("토큰 저장에 실패하여 서비스 이용이 어려울듯 싶습니다..")
-        })
+        /**
+         * 설명
+         * - 모종의 이유로 첫번째 getToken 요청은 거의 실패한다.
+         * - 따라서, try/catch 로 첫번째 요청의 실패를 두번째 요청으로 보내게끔 처리하였다.
+         * */
+        try {
+            return await tokenProcess();
+        } catch (e) {
+            try {
+                return await tokenProcess();
+            } catch (e) {
+                console.error("토큰을 발급받던 중 오류 발생");
+                throw new Error("토큰 발행 과정에서 오류가 발생하였습니다. 다시 시도하여주세요.");
+            }
+        }
+
     }
-  }
 
-  return (
-    <>
-      <h2>Night Saver</h2>
-      <p>토큰 발행시 알림을 허용해주셔야 동작합니다.</p>
+    /**
+     * 설명
+     * - getToken 이 첫번째 시도에서 실패할 수 있기 때문에 try/catch 로 감싸주지 않았다.
+     * */
+    const tokenProcess = async () => {
+        const token = await getToken(messaging, {
+            vapidKey: process.env.REACT_APP_VAPID_KEY,
+        });
 
-      <button onClick={issueToken}>
-        토큰 발행
-      </button>
-    </>
-  )
+        console.log("토큰을 발행하였습니다. : ", token);
+
+        return token;
+    }
+
+    const saveToken = async (token) => {
+
+        const url = "http://localhost:8080/api/v1/token";
+        const body = {
+            token: token
+        };
+
+        try{
+            await axios.post(url, body);
+            console.log("토큰을 저장하였습니다.");
+            alert("토큰이 발행되어 서비스를 사용하실 수 있습니다.");
+        }
+        catch (e){
+            console.error("토큰 저장에 실패하였습니다.");
+            throw new Error("토큰 저장에 실패하였습니다.");
+        }
+    }
+
+    return (
+        <>
+            <p>토큰 발행시 알림을 허용해주셔야 동작합니다.</p>
+
+            <button onClick={issueToken}>
+                토큰 발행
+            </button>
+        </>
+    )
 }
 
 export default NightSaver;
