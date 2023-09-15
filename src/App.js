df7e1dd86fb5b3e2d62b55d853873530b42e2d12
sggnology <sggnology@gmail.com>
2023-09-15 18:13:53 +0900

    [view] app, root 경로에 NightSaver 가 위치하게끔 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 // import './firebase-messaging-sw.js'
 import Header from "./page/layout/Header";
-import Main from "./page/layout/Main";
 import {Route, Routes} from "react-router-dom";
 import NotFound from "./page/exception/NotFound";
 import NightSaver from "./page/view/nightSaver/NightSaver";
@@ -11,8 +10,8 @@ function App() {
     <div className="App">
       <Header/>
       <Routes>
-        <Route path="/" element={<Main/>}></Route>
-        <Route path="/nightSaver" element={<NightSaver/>}></Route>
+        <Route path="/" element={<NightSaver/>}></Route>
+        {/*<Route path="/nightSaver" element={<NightSaver/>}></Route>*/}
         {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
