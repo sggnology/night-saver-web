170fe9441cd41703b7fb80b39bced90eb0384316
sggnology <sggnology@gmail.com>
2023-10-21 01:10:56 +0900

    [service] 로그인된 상태에서 signin, signup 에 접근하지 못하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from "./page/layout/Header";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import NotFound from "./page/exception/NotFound";
 import NightSaver from "./page/view/nightSaver/NightSaver";
 import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import SignIn from "./page/view/signin/SignIn";
 import Landing from "./page/view/landing/Landing";
 import SignUp from "./page/view/signup/SignUp";
+import {useSelector} from "react-redux";
 
 const theme = createTheme({
   typography: {
@@ -17,6 +18,9 @@ const theme = createTheme({
 });
 
 function App() {
+
+  const {authenticated} = useSelector((state) => state.token);
+
   return (
     <div className="App">
       <CssBaseline/>
@@ -25,8 +29,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing/>}></Route>
           <Route path="/nightSaver" element={<NightSaver/>}></Route>
-          <Route path="/signin" element={<SignIn/>}></Route>
-          <Route path="/signup" element={<SignUp/>}></Route>
+          <Route
+            path="/signin"
+            element={authenticated ? <Navigate to={"/"}/> : <SignIn/>}
+          />
+          <Route
+            path="/signup"
+            element={authenticated ? <Navigate to={"/"}/> : <SignUp/>}
+          />
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
           <Route path="*" element={<NotFound/>}></Route>
         </Routes>
