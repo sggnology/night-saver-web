bd62e41a599ddd01201096e2a0796d58043047d9
sggnology <sggnology@gmail.com>
2023-10-23 01:14:01 +0900

    [service] config > api, AxiosInstance 의 interceptor 등록
    - 401 인증 오류 반환시 signin 페이지로 이동하게끔 처리

--- src/config/api/AxiosInstance.js
+++ src/config/api/AxiosInstance.js
@@ -2,6 +2,14 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}`,
+})
+
+axiosInstance.interceptors.response.use(function(response){
+  if(response.data.code === 401){
+    alert("로그인이 필요합니다.");
+    window.location.replace("/signin");
+  }
 });
 
 export default axiosInstance;
+
