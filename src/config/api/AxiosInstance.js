3f90a3ecc1cd2341ebeb99a0bc63cc14ca440101
sggnology <sggnology@gmail.com>
2023-10-24 01:29:29 +0900

    [config] api, AxiosInsance 에 대해 자격 인증 요청하기 위한 설정
    - withCredentials: true

--- src/config/api/AxiosInstance.js
+++ src/config/api/AxiosInstance.js
@@ -4,6 +4,8 @@ const axiosInstance = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}`,
 })
 
+axiosInstance.defaults.withCredentials = true;
+
 axiosInstance.interceptors.response.use(function(response){
   if(response.data.code === 401){
     alert("로그인이 필요합니다.");
