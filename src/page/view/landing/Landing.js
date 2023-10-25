2e3ccfb67db80e90b4e7530118f3cc1e9361916b
sggnology <sggnology@gmail.com>
2023-10-25 22:23:02 +0900

    [view] landing, Landing 의 alignItems 를 center 로 수정

--- src/page/view/landing/Landing.js
+++ src/page/view/landing/Landing.js
@@ -10,40 +10,17 @@ function Landing() {
   return (
     <Container maxWidth={false} disableGutters={true}>
       <Box sx={{
+        width: '100%',
+        height: 'calc(100vh - 50px)',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        width: '100%',
-        height: 'calc(100vh - 50px)',
+        alignItems: 'center',
         backgroundColor: '#9eb7f6',
       }}>
         {
           authenticated ? <AuthenticatedLanding/> : <NotAuthenticatedLanding/>
         }
-        {/*<Typography variant="h2" gutterBottom>*/}
-        {/*  Night Saver*/}
-        {/*</Typography>*/}
-        {/*<Typography variant="body1" gutterBottom>*/}
-        {/*  Prevent Danger Of Night Driving*/}
-        {/*</Typography>*/}
-        {/*<Box*/}
-        {/*  sx={{*/}
-        {/*    display: 'flex',*/}
-        {/*    flexDirection: 'row',*/}
-        {/*    justifyContent: 'center',*/}
-        {/*    alignItems: 'center',*/}
-        {/*    width: '100%',*/}
-        {/*    height: '50px',*/}
-        {/*  }}*/}
-        {/*>*/}
-        {/*  <Button*/}
-        {/*    variant="contained"*/}
-        {/*    onClick={handleStartButton}*/}
-        {/*    sx={{*/}
-        {/*      width: '100px'*/}
-        {/*    }}*/}
-        {/*  >시작하기</Button>*/}
-        {/*</Box>*/}
       </Box>
     </Container>
   )
