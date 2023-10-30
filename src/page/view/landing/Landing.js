4941eeb3db70e41814cf637ee232250289d33355
sggnology <sggnology@gmail.com>
2023-10-31 00:33:31 +0900

    [view] landing, 최상위 Container 의 flex 설정값 추가
    - direction 및 justifyContent: center 로 설정

--- src/page/view/landing/Landing.js
+++ src/page/view/landing/Landing.js
@@ -12,6 +12,8 @@ function Landing() {
       maxWidth={false} disableGutters={true}
       sx={{
         display: 'flex',
+        direction: 'column',
+        justifyContent: 'center',
         minHeight: 'calc(100vh - 50px)',
         backgroundColor: '#9eb7f6',
       }}
