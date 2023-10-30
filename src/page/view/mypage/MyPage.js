96e6bd66f527a728db29d83460536e209811c596
sggnology <sggnology@gmail.com>
2023-10-30 23:39:58 +0900

    [view] mypage, Tab 적용

--- src/page/view/mypage/MyPage.js
+++ src/page/view/mypage/MyPage.js
@@ -1,30 +1,44 @@
-import {Box, Container, Divider, Stack, Typography} from "@mui/material";
+import {Box, Container, Divider, Stack, Tab, Tabs, Typography} from "@mui/material";
+import {TabContext, TabList, TabPanel} from "@mui/lab";
 import MyPageInfo from "./info/MyPageInfo";
 import MyPageSettings from "./settings/MyPageSettings";
+import React, {useState} from "react";
 
 function MyPage() {
+
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Container maxWidth="xs"
                sx={{
                  display: 'flex',
                  flexDirection: 'column',
-                 justifyContent: 'center',
                  alignItems: 'center',
                  height: 'calc(100vh - 50px)'
                }}
     >
-      <Typography variant="h5" sx={{mb: 4}}>
-        My Page
-      </Typography>
-
-      <Stack
-        spacing={2}
-        divider={<Divider orientation="horizontal" flexItem />}
-      >
-        <MyPageInfo/>
-        <MyPageSettings/>
-      </Stack>
+      <TabContext value={value}>
+        <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="My Page" value="1"/>
+            <Tab label="Report Log" value="2"/>
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <Stack
+            spacing={2}
+            divider={<Divider orientation="horizontal" flexItem/>}
+          >
+            <MyPageInfo/>
+            <MyPageSettings/>
+          </Stack>
+        </TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+      </TabContext>
     </Container>
   )
 }
