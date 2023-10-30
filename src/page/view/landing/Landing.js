60a490f408b2a7d7bb8951d73e77682c728d0df6
sggnology <sggnology@gmail.com>
2023-10-30 23:36:15 +0900

    [view] landing, Box 컴포넌트를 삭제하고 Container 에 style 적용
    - display 를 flex 로 가져가 child dom 의 flex-grow 조정 가능하게 설정

--- src/page/view/landing/Landing.js
+++ src/page/view/landing/Landing.js
@@ -8,20 +8,17 @@ function Landing() {
   const {authenticated} = useSelector((state) => state.token);
 
   return (
-    <Container maxWidth={false} disableGutters={true}>
-      <Box sx={{
-        width: '100%',
-        height: 'calc(100vh - 50px)',
+    <Container
+      maxWidth={false} disableGutters={true}
+      sx={{
         display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+        minHeight: 'calc(100vh - 50px)',
         backgroundColor: '#9eb7f6',
-      }}>
-        {
-          authenticated ? <AuthenticatedLanding/> : <NotAuthenticatedLanding/>
-        }
-      </Box>
+      }}
+    >
+      {
+        authenticated ? <AuthenticatedLanding/> : <NotAuthenticatedLanding/>
+      }
     </Container>
   )
 }
