92ac3d0f4a40cbc6c24fe40cd1923c5e5dd2be08
sggnology <sggnology@gmail.com>
2023-11-01 23:10:52 +0900

    [view] mypage, 2번째 탭을 `Report Log` 로 설정

--- src/page/view/mypage/MyPage.js
+++ src/page/view/mypage/MyPage.js
@@ -3,13 +3,14 @@ import {TabContext, TabList, TabPanel} from "@mui/lab";
 import MyPageInfo from "./info/MyPageInfo";
 import MyPageSettings from "./settings/MyPageSettings";
 import React, {useState} from "react";
+import RankRecord from "../../component/RankRecord";
 
 function MyPage() {
 
-  const [value, setValue] = useState("1");
+  const [tabValue, setTabValue] = useState("1");
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    setTabValue(newValue);
   };
 
   return (
@@ -20,11 +21,11 @@ function MyPage() {
                  height: 'calc(100vh - 50px)'
                }}
     >
-      <TabContext value={value}>
+      <TabContext value={tabValue}>
         <Box sx={{borderColor: 'divider'}}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="My Page" value="1"/>
-            {/*<Tab label="Report Log" value="2"/>*/}
+            <Tab label="Report Log" value="2"/>
           </TabList>
         </Box>
         <TabPanel value="1">
@@ -36,7 +37,9 @@ function MyPage() {
             <MyPageSettings/>
           </Stack>
         </TabPanel>
-        {/*<TabPanel value="2">Item Two</TabPanel>*/}
+        <TabPanel value="2">
+          <RankRecord type={"user"} reRenderCount={0}/>
+        </TabPanel>
       </TabContext>
     </Container>
   )
